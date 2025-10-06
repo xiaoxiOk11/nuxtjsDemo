9670fd1f6fddc4d327b4400cf8c8416725333835
xiaoxiOk11 <xiaoxi112233ok@gmail.com>
2025-10-07 02:04:03 +0800

    update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,7 +11,9 @@ export default defineNuxtConfig({
     port: 3001,
     host: "localhost",
   },
-
+  build: {
+    transpile: ['vue-qrcode', 'qrcode'],
+  },
   // ------------------------------------------------------------------
   // 删掉整个 'components' 配置块，因为它已经被 @element-plus/nuxt 自动处理了
   // ------------------------------------------------------------------
